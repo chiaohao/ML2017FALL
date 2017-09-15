c37ff5d6687d259e2f8577902fead4482154e712
chiaohao <ksjoey0096@gmail.com>
2017-09-15 23:36:40 +0800

    Q2 output png

--- hw0/Q2.py
+++ hw0/Q2.py
@@ -10,6 +10,6 @@ for i in range(img_rgb.size[0]):
     for j in range(img_rgb.size[1]):
     	r, g, b = pixels[i, j]
     	pixels[i, j] = (int(r/2), int(g/2), int(b/2))
-img_rgb.save("Q2.jpg")
+img_rgb.save("Q2.png", "PNG")
 
 
