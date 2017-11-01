764e74c894edfc84bc631c98e6d5baeea9d4e61c
Szu-Chiao Hao <chiaohao@cml24.csie.ntu.edu.tw>
2017-11-01 16:26:04 +0800

    fix output bugs

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,56 +1,74 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Flatten, Dropout
 from keras.optimizers import Adam
+from keras.utils import np_utils
 
 import csv
 import numpy as np
 import sys
 
-xs = []
-ys = []
+xs_train = []
+ys_train = []
 xs_test = []
+ys_test = []
+xs_predict = []
 with open(sys.argv[1]) as csvfile:
     r = list(csv.reader(csvfile))
     r = r[1:]
-    ys_int = [int(i[0]) for i in r]
-    ys = []
-    for y in ys_int:
-        ys.append([])
-        c = len(ys) - 1
-        for i in range(7):
-            if y == i:
-                ys[c].append(1)
-            else:
-                ys[c].append(0)
+    ys = np_utils.to_categorical([int(i[0]) for i in r])
     xs = np.asarray([[int(j) for j in i[1].split(' ')] for i in r])
     xs = xs.reshape(-1,48,48,1) / 255.0
+    n = int(float(ys.shape[0]) * 0.7)
+    xs_train = xs[:n]
+    xs_test = xs[n:]
+    ys_train = ys[:n]
+    ys_test = ys[n:]
 
 with open(sys.argv[2]) as csvfile2:
     r = list(csv.reader(csvfile2))
     r = r[1:]
-    xs_test = np.asarray([[int(j) for j in i[1].split(' ')] for i in r])
-    xs_test = xs_test.reshape(-1,48,48,1)
+    xs_predict = np.asarray([[int(j) for j in i[1].split(' ')] for i in r])
+    xs_predict = xs_predict.reshape(-1,48,48,1) / 255.0
 
 model = Sequential()
-model.add(Convolution2D(52,5,5,input_shape=(48,48,1)))
-model.add(Activation('relu'))
+
+model.add(Convolution2D(50,4,4,input_shape=(48,48,1), activation='relu'))
 model.add(MaxPooling2D((2,2)))
-model.add(Convolution2D(104,5,5))
-model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(100,4,4, activation='relu'))
 model.add(MaxPooling2D((2,2)))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(200,4,4, activation='relu'))
+model.add(MaxPooling2D((2,2)))
+model.add(Dropout(0.5))
+
 model.add(Flatten())
-model.add(Dense(1024))
-model.add(Activation('relu'))
-model.add(Dense(7))
-model.add(Activation('softmax'))
+
+model.add(Dense(800, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(200, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+model.summary()
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(xs, ys, batch_size=52, epochs=20)
-score = model.evaluate(xs, ys)
+model.fit(x=xs_train, y=ys_train, batch_size=200, epochs=50)
+
+score = model.evaluate(xs_train, ys_train)
+print('\nTraining Set')
 print('Loss: ', score[0])
 print('Accuracy: ', score[1])
 
-result = model.predict(xs_test)
-result = np.sum(result * np.array([0,1,2,3,4,5,6]), axis=1)
+score = model.evaluate(xs_test, ys_test)
+print('\nTesting Set')
+print('Loss: ', score[0])
+print('Accuracy: ', score[1])
+
+result = model.predict(xs_predict)
+result = [r.argmax() for r in result]
 
 w = []
 for idx, i in enumerate(result):
